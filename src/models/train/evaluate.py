eb1b74579cceb6a3a36024023db72bc0ec04f399
kimtaewan22 <mytae1@naver.com>
2023-11-30 16:29:22 +0000

    feat/평가 함수 추가

---
+++ src/models/train/evaluate.py
@@ -0,0 +1,12 @@
+def evaluate(model, test_loader):
+    model.eval()
+    correct, total = 0, 0
+    with torch.no_grad():
+        for batch in test_loader:
+            input_ids = batch['input_ids'].to(model.device)
+            labels = batch['labels'].to(model.device)
+            outputs = model(input_ids)
+            _, predicted = torch.max(outputs.logits, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+    return correct / total
