7ecd7d08d7848937f23a15d0006559ee440a65d3
kimtaewan22 <mytae1@naver.com>
2023-12-12 12:20:35 +0000

    feat/text clean함수 변경 후 모델 학습 진행

---
+++ src/models/train/util/text_cleaning_1.py
@@ -0,0 +1,24 @@
+# import re
+# import emoji
+# from soynlp.normalizer import repeat_normalize
+
+# emojis = ''.join(emoji.UNICODE_EMOJI.keys())
+# pattern = re.compile(f'[^ .,?!/@$%~％·∼()\x00-\x7Fㄱ-ㅣ가-힣{emojis}]+')
+# url_pattern = re.compile(
+#     r'https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)')
+
+import re
+import emoji
+from soynlp.normalizer import repeat_normalize
+
+pattern = re.compile(f'[^ .,?!/@$%~％·∼()\x00-\x7Fㄱ-ㅣ가-힣]+')
+url_pattern = re.compile(
+    r'https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)')
+
+def clean(x): 
+    x = pattern.sub(' ', x)
+    x = emoji.replace_emoji(x, replace='') #emoji 삭제
+    x = url_pattern.sub('', x)
+    x = x.strip()
+    x = repeat_normalize(x, num_repeats=2)
+    return x
